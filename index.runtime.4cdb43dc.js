72f2c4c20c46170cd1931a1e871aa4c0a5f7b07e
eonarheim <erik.onarheim@gmail.com>
2024-05-05 05:59:44 +0000

    Deploy game

---
+++ index.runtime.4cdb43dc.js
@@ -0,0 +1,67 @@
+
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+      var $parcel$global = globalThis;
+    
+var $parcel$modules = {};
+var $parcel$inits = {};
+
+var parcelRequire = $parcel$global["parcelRequire809a"];
+
+if (parcelRequire == null) {
+  parcelRequire = function(id) {
+    if (id in $parcel$modules) {
+      return $parcel$modules[id].exports;
+    }
+    if (id in $parcel$inits) {
+      var init = $parcel$inits[id];
+      delete $parcel$inits[id];
+      var module = {id: id, exports: {}};
+      $parcel$modules[id] = module;
+      init.call(module.exports, module, module.exports);
+      return module.exports;
+    }
+    var err = new Error("Cannot find module '" + id + "'");
+    err.code = 'MODULE_NOT_FOUND';
+    throw err;
+  };
+
+  parcelRequire.register = function register(id, init) {
+    $parcel$inits[id] = init;
+  };
+
+  $parcel$global["parcelRequire809a"] = parcelRequire;
+}
+
+var parcelRegister = parcelRequire.register;
+parcelRegister("aKzDW", function(module, exports) {
+
+$parcel$export(module.exports, "register", () => $7d39d93f9098a310$export$6503ec6e8aabbaf, (v) => $7d39d93f9098a310$export$6503ec6e8aabbaf = v);
+var $7d39d93f9098a310$export$6503ec6e8aabbaf;
+var $7d39d93f9098a310$export$f7ad0328861e2f03;
+"use strict";
+var $7d39d93f9098a310$var$mapping = new Map();
+function $7d39d93f9098a310$var$register(baseUrl, manifest) {
+    for(var i = 0; i < manifest.length - 1; i += 2)$7d39d93f9098a310$var$mapping.set(manifest[i], {
+        baseUrl: baseUrl,
+        path: manifest[i + 1]
+    });
+}
+function $7d39d93f9098a310$var$resolve(id) {
+    var resolved = $7d39d93f9098a310$var$mapping.get(id);
+    if (resolved == null) throw new Error("Could not resolve bundle with id " + id);
+    return new URL(resolved.path, resolved.baseUrl).toString();
+}
+$7d39d93f9098a310$export$6503ec6e8aabbaf = $7d39d93f9098a310$var$register;
+$7d39d93f9098a310$export$f7ad0328861e2f03 = $7d39d93f9098a310$var$resolve;
+
+});
+
+var $2d81fc3681422e35$exports = {};
+
+(parcelRequire("aKzDW")).register(new URL("", import.meta.url).toString(), JSON.parse('["h1FzW","index.743d8ff1.js","d7UW8","tilemap_packed.da7b5a4c.png","lr3SA","map.8063a5e3.tmx"]'));
+
+
+//# sourceMappingURL=index.runtime.4cdb43dc.js.map
